a33154bf2f5f324408d6aef77df0e69f25df8d57
Xavier Fraboulet <xavier@xavier.home>
2013-12-07 16:50:21 +0100

    WPF Application created

---
+++ diagramme/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMap
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<int> map;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            map = new List<int>();
+            int width = 4;
+            int height = 4;
+            for (int i = 0; i < 16; i++)
+            {
+                map.Add(i % 5);
+            }
+
+            for (int c = 0; c < width; c++)
+            {
+                mapGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50, GridUnitType.Pixel) });
+            }
+
+            for (int l = 0; l < height; l++)
+            {
+                mapGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(50, GridUnitType.Pixel) });
+
+                for (int c = 0; c < width; c++)
+                {
+                    int type = map[l * width + c];
+                    var rect = createRectangle(type, c, l);
+                    mapGrid.Children.Add(rect);
+
+                }
+            }
+        }
+
+        private Rectangle createRectangle(int type, int c, int l)
+        {
+            var rectangle = new Rectangle();
+            rectangle.Fill = Brushes.Beige;
+            Grid.SetColumn(rectangle, c);
+            Grid.SetRow(rectangle, l);
+            rectangle.Tag = c * 4 + l;
+            rectangle.Stroke = Brushes.Red;
+            rectangle.StrokeThickness = 1;
+
+
+
+
+            return rectangle;
+        }
+    }
+}
