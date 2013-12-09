bec55e76a1c55055371cedcc39a266ca571ade16
Xavier Fraboulet <xavier@xavier.res.insa>
2013-12-09 10:14:20 +0100

    FactoryImage added for the GUI

---
+++ diagramme/GUI/Class/ImageFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using SmallWorld;
+
+namespace GUI
+{
+    class ImageFactory
+    {
+        private static BitmapImage tileDesert = null;
+        private static BitmapImage tileEau = null;
+        private static BitmapImage tileForet = null;
+        private static BitmapImage tileMontagne = null;
+        private static BitmapImage tilePlaine = null;
+
+
+
+        public static Brush getBrush(ISquare square)
+        {
+            ImageBrush brush = new ImageBrush();
+
+            if (square is IDesert)
+            {
+                if (tileDesert == null)
+                    tileDesert = new BitmapImage(new Uri(@"..\..\Ressources\terrains\desert.png", UriKind.Relative));
+                brush.ImageSource = tileDesert;
+            }
+            else if (square is ISea)
+            {
+                if (tileEau == null)
+                    tileEau = new BitmapImage(new Uri(@"..\..\Ressources\terrains\sea.png", UriKind.Relative));
+                brush.ImageSource = tileEau;
+            }
+            else if (square is IForest)
+            {
+                if (tileForet == null)
+                    tileForet = new BitmapImage(new Uri(@"..\..\Ressources\terrains\forest.png", UriKind.Relative));
+                brush.ImageSource = tileForet;
+            }
+            else if (square is IMountain)
+            {
+                if (tileMontagne == null)
+                    tileMontagne = new BitmapImage(new Uri(@"..\..\Ressources\terrains\mountain.png", UriKind.Relative));
+                brush.ImageSource = tileMontagne;
+            }
+            else if (square is ILowland)
+            {
+                if (tilePlaine == null)
+                    tilePlaine = new BitmapImage(new Uri(@"..\..\Ressources\terrains\lowland.png", UriKind.Relative));
+                brush.ImageSource = tilePlaine;
+            }
+            return brush;
+        }
+    }
+}
---
+++ diagramme/GUI/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Point = System.Drawing.Point;
+using Bitmap = System.Drawing.Bitmap;
+using Image = System.Drawing.Image;
+using TextureBrush = System.Drawing.TextureBrush;
+using Graphics = System.Drawing.Graphics;
+using SmallWorld;
+using GUI;
+
+namespace WpfMap
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dictionary<Point, ISquare> squares = new Dictionary<Point, ISquare>();
+            int size = 15;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    squares.Add(new Point(i, j), new Desert());
+                }
+            }
+            IMap map = new Map(squares);
+            map.setSize(15);
+
+            size = map.getSize();
+
+            for (int c = 0; c < size; c++)
+            {
+                mapGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50, GridUnitType.Pixel) });
+            }
+
+            for (int l = 0; l < size; l++)
+            {
+                mapGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(50, GridUnitType.Pixel) });
+
+                for (int c = 0; c < size; c++)
+                {
+                    ISquare type = map.getSquare(new Point(l, c));
+                    var rect = createRectangle(type, c, l);
+                    mapGrid.Children.Add(rect);
+
+                }
+            }
+        }
+
+        private Rectangle createRectangle(ISquare type, int c, int l)
+        {
+            var rectangle = new Rectangle();
+
+
+
+            rectangle.Fill = ImageFactory.getBrush(type);
+            Grid.SetColumn(rectangle, c);
+            Grid.SetRow(rectangle, l);
+            rectangle.Tag = c * 4 + l;
+            //rectangle.Stroke = Brushes.Red;
+            rectangle.StrokeThickness = 1;
+            
+            return rectangle;
+        }
+    }
+}
